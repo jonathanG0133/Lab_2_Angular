3f9ff1b3c7f1a61520da08d72ed48beec0a287aa
Jonathan G <reacct1@hotmail.com>
2023-09-23 21:15:54 +0200

    adderat html filer för varje komponent.ts-fil och flyttat html-koden dit för möjligheten att hänvisa till dessa filer med hjälp av "templateUrl"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,18 +9,7 @@ import { RouterModule } from '@angular/router';
     HomeComponent,
     RouterModule,
   ],
-  template: `
-  <main>
-    <a [routerLink]="['/']">
-      <header class="brand-name">
-        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
-      </header>
-    </a>
-    <section class="content">
-      <router-outlet></router-outlet>
-    </section>
-  </main>
-`,
+  templateUrl: "./app.component.html",
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
